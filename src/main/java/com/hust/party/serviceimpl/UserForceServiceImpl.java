b14a3d85269d5e134c3b0976991d23e1e0c745e8
liyue <286311613@qq.com>
2018-06-08 14:50:18 +0800

    团力值

--- src/main/java/com/hust/party/serviceimpl/UserForceServiceImpl.java
+++ src/main/java/com/hust/party/serviceimpl/UserForceServiceImpl.java
@@ -39,6 +39,12 @@ public class UserForceServiceImpl extends  AbstractBaseServiceImpl<UserForce> im
                     else
                         userForce.setUserForce(userForces.get(0).getUserForce() + 2);
                     userForce.setUpdateTime(new Date());
+                    if( userForce.getUserForce()>50)
+                        userForce.setUserMedal("铜牌团长");
+                    else if( userForce.getUserForce()>200)
+                        userForce.setUserMedal("银牌团长");
+                    else if( userForce.getUserForce()>500)
+                        userForce.setUserMedal("金牌团长");
                     Integer insert = userForceMapper.updateByPrimaryKeySelective(userForce);
                     if (insert != null)
                         insertNum = 1;
@@ -61,6 +67,14 @@ public class UserForceServiceImpl extends  AbstractBaseServiceImpl<UserForce> im
         if(userForces.size()!=0) {
             userForce.setId(userForces.get(0).getId());
             userForce.setUserForce(userForces.get(0).getUserForce() + 10);
+
+            if( userForce.getUserForce()>50)
+                userForce.setUserMedal("铜牌团长");
+            else if( userForce.getUserForce()>200)
+                userForce.setUserMedal("银牌团长");
+            else if( userForce.getUserForce()>500)
+                userForce.setUserMedal("金牌团长");
+
             Integer insert = userForceMapper.updateByPrimaryKeySelective(userForce);
             if (insert != null)
                 insertNum = 1;
@@ -81,6 +95,12 @@ public class UserForceServiceImpl extends  AbstractBaseServiceImpl<UserForce> im
         if(userForces.size()!=0) {
             userForce.setId(userForces.get(0).getId());
             userForce.setUserForce(userForces.get(0).getUserForce() + 2);
+            if( userForce.getUserForce()>50)
+                userForce.setUserMedal("铜牌团长");
+            else if( userForce.getUserForce()>200)
+                userForce.setUserMedal("银牌团长");
+            else if( userForce.getUserForce()>500)
+                userForce.setUserMedal("金牌团长");
             Integer insert = userForceMapper.updateByPrimaryKeySelective(userForce);
             if (insert != null)
                 insertNum = 1;
